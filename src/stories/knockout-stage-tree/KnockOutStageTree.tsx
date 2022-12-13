d8d2a70a8de609642557796c7c6d137e61bda3d7
Houzair Koussa <houzairmk@icloud.com>
2022-12-13 11:29:41 +0400

    [dev] ADD column layout

--- src/stories/knockout-stage-tree/KnockOutStageTree.tsx
+++ src/stories/knockout-stage-tree/KnockOutStageTree.tsx
@@ -6,13 +6,40 @@ interface KnockOutStageTreeProps {
 
 const KnockOutStageTree: React.FunctionComponent<KnockOutStageTreeProps> = () => {
   return (
-    <StyleWrapper>
+    <StyleWrapper className="knock-out-stage-tree">
+      <div className="knock-out-stage-tree-section-1">
+      </div>
+      <div className="knock-out-stage-tree-section-2">
+      </div>
+      <div className="knock-out-stage-tree-section-1">
+      </div>
+      <div className="knock-out-stage-tree-section-2">
+      </div>
     </StyleWrapper>
   );
 }
 
 const StyleWrapper = styled.div`
-  
+  &.knock-out-stage-tree {
+    height: 100vh;
+    width: 100vw;
+    display: flex;
+    padding: 0;
+    margin: 0;
+    overflow: scroll;
+
+    .knock-out-stage-tree-section-1 {
+      background-color: red;
+      height: 100%;
+      min-width: 50%;
+    }
+
+    .knock-out-stage-tree-section-2 {
+      background-color: blue;
+      height: 100%; 
+      min-width: 50%;
+    }
+  }
 `
 
 export default KnockOutStageTree;
